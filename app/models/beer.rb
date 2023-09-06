78c65e268e20931d2a8e836bfc44fc80ccd2eb67
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-09-06 13:14:06 +0300

    2.3-2.6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,12 @@
 class Beer < ApplicationRecord
   belongs_to :brewery
   has_many :ratings
+
+  def average_rating
+    if ratings.empty?
+      return nil
+    else
+      return (ratings.sum(:score) / ratings.count).to_f
+    end
+  end
 end
