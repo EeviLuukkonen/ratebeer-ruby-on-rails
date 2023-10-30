d5054bac4f8a4b0f7cfb468a0c7e8bf5daa7b754
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-10-30 13:39:31 +0200

    3.7

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,5 +1,8 @@
 class Beer < ApplicationRecord
   include RatingAverage
+
+  validates :name, presence: true
+
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
   def to_s
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   include RatingAverage
+
+  validates :username, uniqueness: true
+  validates :username, length: { minimum: 3,
+                                 maximum: 30 }
+
   has_many :ratings
 end
