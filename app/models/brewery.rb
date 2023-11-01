79db0159522d4ac6cc0d72fe03f97c34dc0b1ff0
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-11-01 15:27:35 +0200

    3.8

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,9 +2,15 @@ class Brewery < ApplicationRecord
   include RatingAverage
 
   validates :name, presence: true
-  validates :year, numericality: { greater_than_or_equal_to: 1040,
-                                   less_than_or_equal_to: 2023 }
+  validates :year, numericality: { greater_than_or_equal_to: 1040 }
+  validate :year_cannot_be_in_the_future
 
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  def year_cannot_be_in_the_future
+    if year > Time.now.year
+      errors.add(:year, "year cannot be in the future!")
+    end
+  end
 end
