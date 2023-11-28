ec616d20d25fee1a8dedd8d9723efe8ff00452bf
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-11-28 14:11:04 +0200

    rubocop fix

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -9,8 +9,8 @@ class Brewery < ApplicationRecord
   has_many :ratings, through: :beers
 
   def year_cannot_be_in_the_future
-    if year > Time.now.year
-      errors.add(:year, "year cannot be in the future!")
-    end
+    return unless year > Time.now.year
+
+    errors.add(:year, "year cannot be in the future!")
   end
 end
