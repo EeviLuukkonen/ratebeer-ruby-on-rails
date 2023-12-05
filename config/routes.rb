bcd98d04e48e6785f7c7151a8ee2d3b2f1017e03
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-12-05 12:44:44 +0200

    5.1

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'ratings/new', to: 'ratings#new'
   #post 'ratings', to: 'ratings#create'
   get 'signin', to: 'sessions#new'
+  get 'places', to: 'places#index'
+  post 'places', to: 'places#search'
   delete 'signout', to: 'sessions#destroy'
   resources :ratings, only: [:index, :new, :create]
   resources :ratings, only: [:index, :new, :create, :destroy]
