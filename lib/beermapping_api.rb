a4901d641af22529da30b120339f0b3d357cfc10
EeviLuukkonen <eevi.luukkonen@gmail.com>
2023-12-05 16:04:38 +0200

    5.4

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,5 +1,10 @@
 class BeermappingApi
   def self.places_in(city)
+    city = city.downcase
+    Rails.cache.fetch(city, expires_in: 1.week) { get_places_in(city) }
+  end
+
+  def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
@@ -14,6 +19,9 @@ class BeermappingApi
   end
 
   def self.key
-    "24110af4f844956306c4ac25dc1cdc12"
+    return nil if Rails.env.test? # testatessa ei apia tarvita, palautetaan nil
+    raise 'BEERMAPPING_APIKEY env variable not defined' if ENV['BEERMAPPING_APIKEY'].nil?
+
+    ENV.fetch('BEERMAPPING_APIKEY')
   end
 end
